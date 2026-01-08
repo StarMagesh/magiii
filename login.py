9b4e06806c85c19b67a3b55204e43a33911fe441
Gunal-23 <dheeraj.v2023@vitstudent.ac.in>
2026-01-08 17:01:24 +0530

    added login file

---
+++ login.py
@@ -0,0 +1,8 @@
+name="admin"
+password="1234"
+name1=input("enter a name:")
+password1=input("enter password:")
+if name==name1 and password ==password1:
+    print("login successful")
+else:
+    print("Invalid username or password")
